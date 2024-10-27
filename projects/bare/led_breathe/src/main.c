49db27ba40fd0fcb582941bb8eac2ab828e38ef0
MoonGrt <1561145394@qq.com>
2024-10-27 15:44:23 +0800

    update

--- projects/bare/led_breathe/src/main.c
+++ projects/bare/led_breathe/src/main.c
@@ -3,7 +3,7 @@
 /* 软件延时 */
 void delay(unsigned int count)
 {
-    count *= 50000;
+    count *= 10000;
     for (; count != 0; count--)
         ;
 }
@@ -53,9 +53,9 @@ void PWM_init(void)
     /*GPIO初始化*/
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOA, &GPIO_InitStructure); // 将PA8引脚初始化为复用推挽输出
+    GPIO_Init(GPIOA, &GPIO_InitStructure); // 将PA15引脚初始化为复用推挽输出
                                            // 受外设控制的引脚，均需要配置为复用模式
 
     /*配置时钟源*/
@@ -65,8 +65,8 @@ void PWM_init(void)
     TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;              // 定义结构体变量
     TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;     // 时钟分频，选择不分频，此参数用于配置滤波器时钟，不影响时基单元功能
     TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up; // 计数器模式，选择向上计数
-    TIM_TimeBaseInitStructure.TIM_Period = 50 - 1;                  // 计数周期，即ARR的值
-    TIM_TimeBaseInitStructure.TIM_Prescaler = 72 - 1;               // 预分频器，即PSC的值
+    TIM_TimeBaseInitStructure.TIM_Period = 100 - 1;                 // 计数周期，即ARR的值
+    TIM_TimeBaseInitStructure.TIM_Prescaler = 50 - 1;               // 预分频器，即PSC的值
     TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;            // 重复计数器，高级定时器才会用到
     TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStructure);             // 将结构体变量交给TIM_TimeBaseInit，配置TIM3的时基单元
 
@@ -79,7 +79,7 @@ void PWM_init(void)
     TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;     // 输出极性，选择为高，若选择极性为低，则输出高低电平取反
     TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable; // 输出使能
     TIM_OCInitStructure.TIM_Pulse = 0;                            // 初始的CCR值
-    TIM_OC1Init(TIM3, &TIM_OCInitStructure);                      // 将结构体变量交给TIM_OC1Init，配置TIM3的输出比较通道1
+    TIM_OC4Init(TIM3, &TIM_OCInitStructure);                      // 将结构体变量交给TIM_OC1Init，配置TIM3的输出比较通道1
 
     /*TIM使能*/
     TIM_Cmd(TIM3, ENABLE); // 使能TIM3，定时器开始运行
@@ -95,13 +95,13 @@ void main()
     {
         for (i = 0; i <= 100; i++)
         {
-            TIM_SetCompare1(TIM3, i); // 依次将定时器的CCR寄存器设置为0~100，PWM占空比逐渐增大，LED逐渐变亮
-            delay(10);          // 延时10ms
+            TIM_SetCompare4(TIM3, i); // 依次将定时器的CCR寄存器设置为0~100，PWM占空比逐渐增大，LED逐渐变亮
+            delay(5);                 // 延时5ms
         }
         for (i = 0; i <= 100; i++)
         {
-            TIM_SetCompare1(TIM3, 100 - i); // 依次将定时器的CCR寄存器设置为100~0，PWM占空比逐渐减小，LED逐渐变暗
-            delay(10);                // 延时10ms
+            TIM_SetCompare4(TIM3, 100 - i); // 依次将定时器的CCR寄存器设置为100~0，PWM占空比逐渐减小，LED逐渐变暗
+            delay(5);                       // 延时5ms
         }
     }
 }
--- projects/rt-thread/test/user/led.c
+++ projects/rt-thread/test/user/led.c
@@ -5,28 +5,73 @@
 void GPIO_init(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
 
+/* PWM 初始化 */
+void PWM_init(void)
+{
+    /*GPIO初始化*/
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure); // 将PA15引脚初始化为复用推挽输出
+                                           // 受外设控制的引脚，均需要配置为复用模式
+
+    /*配置时钟源*/
+    TIM_InternalClockConfig(TIM3); // 选择TIM3为内部时钟，若不调用此函数，TIM默认也为内部时钟
+
+    /*时基单元初始化*/
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;              // 定义结构体变量
+    TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;     // 时钟分频，选择不分频，此参数用于配置滤波器时钟，不影响时基单元功能
+    TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up; // 计数器模式，选择向上计数
+    TIM_TimeBaseInitStructure.TIM_Period = 100 - 1;                 // 计数周期，即ARR的值
+    TIM_TimeBaseInitStructure.TIM_Prescaler = 50 - 1;               // 预分频器，即PSC的值
+    TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;            // 重复计数器，高级定时器才会用到
+    TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStructure);             // 将结构体变量交给TIM_TimeBaseInit，配置TIM3的时基单元
+
+    /*输出比较初始化*/
+    TIM_OCInitTypeDef TIM_OCInitStructure;                        // 定义结构体变量
+    TIM_OCStructInit(&TIM_OCInitStructure);                       // 结构体初始化，若结构体没有完整赋值
+                                                                  // 则最好执行此函数，给结构体所有成员都赋一个默认值
+                                                                  // 避免结构体初值不确定的问题
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;             // 输出比较模式，选择PWM模式1
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;     // 输出极性，选择为高，若选择极性为低，则输出高低电平取反
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable; // 输出使能
+    TIM_OCInitStructure.TIM_Pulse = 0;                            // 初始的CCR值
+    TIM_OC4Init(TIM3, &TIM_OCInitStructure);                      // 将结构体变量交给TIM_OC1Init，配置TIM3的输出比较通道1
+
+    /*TIM使能*/
+    TIM_Cmd(TIM3, ENABLE); // 使能TIM3，定时器开始运行
+}
+
+void led_flow_breathe(int led_index, int increasing) {
+    GPIO_Write(GPIOA, ~(1 << led_index)); // 点亮对应的 LED
+    for (int pulse = 0; pulse <= 100; pulse++) {
+        TIM_SetCompare4(TIM3, increasing ? pulse : 100 - pulse); // 根据 increasing 控制亮度变化
+        rt_thread_delay(5); // 延时 5ms
+    }
+}
+
 /* led 线程 入口函数 */
 void th_led_en(void *p_arg)
 {
+    uint8_t pulse;
+
     GPIO_init();
+    PWM_init();
     printf("Th_led\r\n");
+
     for (;;)
     {
-        /*使用GPIO_Write，同时设置GPIOA所有引脚的高低电平，实现LED流水灯*/
-        GPIO_Write(GPIOA, ~0x0001); // 0000 0000 0000 0001，PA0引脚为低电平，其他引脚均为高电平，注意数据有按位取反
-        rt_thread_delay(500);       // 延时10tick
-        GPIO_Write(GPIOA, ~0x0002); // 0000 0000 0000 0010，PA1引脚为低电平，其他引脚均为高电平
-        rt_thread_delay(500);       // 延时10tick
-        GPIO_Write(GPIOA, ~0x0004); // 0000 0000 0000 0100，PA2引脚为低电平，其他引脚均为高电平
-        rt_thread_delay(500);       // 延时10tick
-        GPIO_Write(GPIOA, ~0x0008); // 0000 0000 0000 1000，PA3引脚为低电平，其他引脚均为高电平
-        rt_thread_delay(500);       // 延时10tick
+        led_flow_breathe(0, 1); // LED 0; 逐渐变亮
+        led_flow_breathe(1, 0); // LED 1; 逐渐变暗
+        led_flow_breathe(2, 1); // LED 2; 逐渐变亮
+        led_flow_breathe(3, 0); // LED 3; 逐渐变暗
     }
 }
 
