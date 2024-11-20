4e4119d4f834de31380861208b61b7332fd0568e
MoonGrt <1561145394@qq.com>
2024-11-20 09:43:55 +0800

    update

--- projects/freertos/test/user/main.c
+++ projects/freertos/test/user/main.c
@@ -56,7 +56,7 @@
 
 /* Set mainCREATE_SIMPLE_BLINKY_DEMO_ONLY to one to run the simple blinky demo,
 or 0 to run the more comprehensive test and demo application. */
-#define mainCREATE_SIMPLE_BLINKY_DEMO_ONLY	1
+#define mainCREATE_SIMPLE_BLINKY_DEMO_ONLY 1
 
 /*-----------------------------------------------------------*/
 
@@ -65,100 +65,101 @@ or 0 to run the more comprehensive test and demo application. */
  * main_full() is used when mainCREATE_SIMPLE_BLINKY_DEMO_ONLY is set to 0.
  */
 #if mainCREATE_SIMPLE_BLINKY_DEMO_ONLY == 1
-	extern void main_blinky( void );
+extern void main_blinky(void);
 #else
-	extern void main_full( void );
+extern void main_full(void);
 #endif /* #if mainCREATE_SIMPLE_BLINKY_DEMO_ONLY == 1 */
 
 /*
  * Prototypes for the standard FreeRTOS callback/hook functions implemented
  * within this file.  See https://www.freertos.org/a00016.html
  */
-void vApplicationMallocFailedHook( void );
-void vApplicationIdleHook( void );
-void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
-void vApplicationTickHook( void );
+void vApplicationMallocFailedHook(void);
+void vApplicationIdleHook(void);
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName);
+void vApplicationTickHook(void);
 
 /*
  * Setup the hardware to run this demo.
  */
-static void prvSetupHardware( void );
+static void prvSetupHardware(void);
 
 /*-----------------------------------------------------------*/
 
-int main( void )
+int main(void)
 {
 	prvSetupHardware();
 
-	/* The mainCREATE_SIMPLE_BLINKY_DEMO_ONLY setting is described at the top
-	of this file. */
-	#if( mainCREATE_SIMPLE_BLINKY_DEMO_ONLY == 1 )
+/* The mainCREATE_SIMPLE_BLINKY_DEMO_ONLY setting is described at the top
+of this file. */
+#if (mainCREATE_SIMPLE_BLINKY_DEMO_ONLY == 1)
 	{
 		main_blinky();
 	}
-	#else
+#else
 	{
 		main_full();
 	}
-	#endif
+#endif
 }
 /*-----------------------------------------------------------*/
 
 /* USART initial */
 void USART_init(void)
 {
-    /*GPIO初始化*/
-    GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOB, &GPIO_InitStructure); // 将PB0引脚初始化为复用推挽输出
+	/*GPIO初始化*/
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure); // 将PB0引脚初始化为复用推挽输出
 
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOB, &GPIO_InitStructure); // 将PB1引脚初始化为上拉输入
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure); // 将PB1引脚初始化为上拉输入
 
-    /*USART初始化*/
-    USART_InitTypeDef USART_InitStructure;                                          // 定义结构体变量
-    USART_InitStructure.USART_BaudRate = 115200;                                    // 波特率
-    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None; // 硬件流控制，不需要  可以不配置，默认为None
-    USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;                 // 模式，发送模式和接收模式均选择
-    USART_InitStructure.USART_Parity = USART_Parity_No;                             // 奇偶校验，不需要
-    USART_InitStructure.USART_StopBits = USART_StopBits_1;                          // 停止位，选择1位
-    USART_InitStructure.USART_WordLength = USART_WordLength_8b;                     // 字长，选择8位
-    USART_Init(USART1, &USART_InitStructure);                                       // 将结构体变量交给USART_Init，配置USART1
-    /*中断输出配置*/
-    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE); // 开启串口接收数据的中断
+	/*USART初始化*/
+	USART_InitTypeDef USART_InitStructure;											// 定义结构体变量
+	USART_InitStructure.USART_BaudRate = 115200;									// 波特率
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None; // 硬件流控制，不需要  可以不配置，默认为None
+	USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;					// 模式，发送模式和接收模式均选择
+	USART_InitStructure.USART_Parity = USART_Parity_No;								// 奇偶校验，不需要
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;							// 停止位，选择1位
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;						// 字长，选择8位
+	USART_Init(USART1, &USART_InitStructure);										// 将结构体变量交给USART_Init，配置USART1
+	/*中断输出配置*/
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE); // 开启串口接收数据的中断
 
-    // /*NVIC中断分组*/
-    // NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); // 配置NVIC为分组2
-    // /*NVIC配置*/
-    // NVIC_InitTypeDef NVIC_InitStructure;                      // 定义结构体变量
-    // NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;         // 选择配置NVIC的USART1线
-    // NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;           // 指定NVIC线路使能
-    // NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1; // 指定NVIC线路的抢占优先级为1
-    // NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;        // 指定NVIC线路的响应优先级为1
-    // NVIC_Init(&NVIC_InitStructure);                           // 将结构体变量交给NVIC_Init，配置NVIC外设
+	// /*NVIC中断分组*/
+	// NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); // 配置NVIC为分组2
+	// /*NVIC配置*/
+	// NVIC_InitTypeDef NVIC_InitStructure;                      // 定义结构体变量
+	// NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;         // 选择配置NVIC的USART1线
+	// NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;           // 指定NVIC线路使能
+	// NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1; // 指定NVIC线路的抢占优先级为1
+	// NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;        // 指定NVIC线路的响应优先级为1
+	// NVIC_Init(&NVIC_InitStructure);                           // 将结构体变量交给NVIC_Init，配置NVIC外设
 
-    /*USART使能*/
-    USART_Cmd(USART1, ENABLE); // 使能USART1，串口开始运行
+	/*USART使能*/
+	USART_Cmd(USART1, ENABLE); // 使能USART1，串口开始运行
 }
 
-static void prvSetupHardware( void )
+static void prvSetupHardware(void)
 {
-    GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+    GPIO_SetBits(GPIOA, GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3);
 
-    USART_init();
-    printf("FreeRTOS\r\n");
+	USART_init();
+	printf("Led_Flow\r\n");
 }
 /*-----------------------------------------------------------*/
 
-void vApplicationMallocFailedHook( void )
+void vApplicationMallocFailedHook(void)
 {
 	/* vApplicationMallocFailedHook() will only be called if
 	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
@@ -171,11 +172,12 @@ void vApplicationMallocFailedHook( void )
 	to query the size of free heap space that remains (although it does not
 	provide information on how the remaining heap might be fragmented). */
 	taskDISABLE_INTERRUPTS();
-	for( ;; );
+	for (;;)
+		;
 }
 /*-----------------------------------------------------------*/
 
-void vApplicationIdleHook( void )
+void vApplicationIdleHook(void)
 {
 	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
 	to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
@@ -189,59 +191,82 @@ void vApplicationIdleHook( void )
 }
 /*-----------------------------------------------------------*/
 
-void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName)
 {
-	( void ) pcTaskName;
-	( void ) pxTask;
+	(void)pcTaskName;
+	(void)pxTask;
 
 	/* Run time stack overflow checking is performed if
 	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
 	function is called if a stack overflow is detected. */
 	taskDISABLE_INTERRUPTS();
-	for( ;; );
+	for (;;)
+		;
 }
 /*-----------------------------------------------------------*/
 
-void vApplicationTickHook( void )
+void vApplicationTickHook(void)
 {
-	/* The tests in the full demo expect some interaction with interrupts. */
-	#if( mainCREATE_SIMPLE_BLINKY_DEMO_ONLY != 1 )
+/* The tests in the full demo expect some interaction with interrupts. */
+#if (mainCREATE_SIMPLE_BLINKY_DEMO_ONLY != 1)
 	{
-		extern void vFullDemoTickHook( void );
+		extern void vFullDemoTickHook(void);
 		vFullDemoTickHook();
 	}
-	#endif
+#endif
 }
 /*-----------------------------------------------------------*/
 
-void vAssertCalled( void )
+void vAssertCalled(void)
 {
-volatile uint32_t ul;
-const uint32_t ulNullLoopDelay = 0x1ffffUL;
+	volatile uint32_t ul;
+	const uint32_t ulNullLoopDelay = 0x1ffffUL;
 
 	taskDISABLE_INTERRUPTS();
 
-	for( ;; )
+	for (;;)
 	{
-		for( ul = 0; ul < ulNullLoopDelay; ul++ )
+		for (ul = 0; ul < ulNullLoopDelay; ul++)
 		{
-			__asm volatile( "nop" );
+			__asm volatile("nop");
 		}
-		GPIOA->ODR ^= 0x1;
+		// GPIOA->ODR ^= 0x1;
 	}
 }
 /*-----------------------------------------------------------*/
 
-void vToggleLED( void )
+void vToggleLED(void)
 {
-#ifdef SIMULATION
-    // 运行成功
-    asm("li x27, 0x01");
-    asm("li x26, 0x01");
-    while (1);
-#else
-    GPIOA->ODR ^= 0x1;
-#endif
+// #ifdef SIMULATION
+// 	// 运行成功
+// 	asm("li x27, 0x01");
+// 	asm("li x26, 0x01");
+// 	while (1)
+// 		;
+// #else
+// 	GPIOA->ODR ^= 0x1;
+// #endif
+    static uint16_t currentLED = 0x01;       // 当前点亮的 LED，初始点亮第一个
+    static uint8_t direction = 1;           // 流水方向：1 表示从左到右，0 表示从右到左
+
+    // 清除所有 LED，然后点亮当前 LED
+    GPIO_Write(GPIOA, GPIOA->ODR & ~((1 << 4) - 1)); // 清除 LED 位
+    GPIO_Write(GPIOA, GPIOA->ODR | currentLED);    // 点亮当前 LED
+
+    // 更新下一次点亮的 LED
+    if (direction) {
+        currentLED <<= 1; // 左移到下一个 LED
+        if (currentLED > (1U << (4 - 1))) {
+            currentLED = (1U << (4 - 2)); // 到达右边界，切换方向
+            direction = 0;
+        }
+    } else {
+        currentLED >>= 1; // 右移到下一个 LED
+        if (currentLED < 0x01) {
+            currentLED = 0x02; // 到达左边界，切换方向
+            direction = 1;
+        }
+    }
 }
 
 /*-----------------------------------------------------------*/
