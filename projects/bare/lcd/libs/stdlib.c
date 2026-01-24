c7b211c319014f23f4dc7ce57fd71bd7ca5dd76f
MoonGrt <1561145394@qq.com>
2026-01-24 14:56:05 +0800

    stdlibs.c add ld/llu/lx printf

--- projects/bare/lcd/libs/stdlib.c
+++ projects/bare/lcd/libs/stdlib.c
@@ -11,6 +11,12 @@ extern int printf(const char *format, ...);
 char heap_memory[1024];
 int heap_memory_used = 0;
 
+void *memset(void *dest, int value, int n) {
+    unsigned char *ptr = (unsigned char *)dest;
+    while (n--) *ptr++ = (unsigned char)value;
+    return dest;
+}
+
 char *malloc(int size)
 {
     char *p = heap_memory + heap_memory_used;
@@ -22,8 +28,18 @@ char *malloc(int size)
 
 void putchar(char c)
 {
-    // uart_write(UART, c);
     USART_SendData(USART1, c);
+}
+
+int puts(char *s)
+{
+    while (*s)
+    {
+        putchar(*s);
+        s++;
+    }
+    putchar('\n');
+    return 0;
 }
 
 static void printf_c(int c)
@@ -55,50 +71,131 @@ static void printf_d(int val)
         printf_c(*(--p));
 }
 
+static void printf_u(unsigned int val)
+{
+    char buffer[32];
+    char *p = buffer;
+    while (val || p == buffer)
+    {
+        *(p++) = '0' + val % 10;
+        val = val / 10;
+    }
+    while (p != buffer)
+        printf_c(*(--p));
+}
+
+static void printf_x(unsigned int val, int uppercase)
+{
+    char buffer[32];
+    char *p = buffer;
+    const char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+    while (val || p == buffer)
+    {
+        *(p++) = digits[val % 16];
+        val = val / 16;
+    }
+    while (p != buffer)
+        printf_c(*(--p));
+}
+
+static void printf_ld(long int val)
+{
+    char buffer[32];
+    char *p = buffer;
+    if (val < 0)
+    {
+        printf_c('-');
+        val = -val;
+    }
+    while (val || p == buffer)
+    {
+        *(p++) = '0' + (val % 10);
+        val = val / 10;
+    }
+    while (p != buffer)
+        printf_c(*(--p));
+}
+
+static void printf_lu(unsigned long int val)
+{
+    char buffer[32];
+    char *p = buffer;
+    while (val || p == buffer)
+    {
+        *(p++) = '0' + (val % 10);
+        val = val / 10;
+    }
+    while (p != buffer)
+        printf_c(*(--p));
+}
+
+static void printf_lx(unsigned long int val, int uppercase)
+{
+    char buffer[32];
+    char *p = buffer;
+    const char *digits = uppercase ?
+        "0123456789ABCDEF" : "0123456789abcdef";
+    while (val || p == buffer)
+    {
+        *(p++) = digits[val % 16];
+        val = val / 16;
+    }
+    while (p != buffer)
+        printf_c(*(--p));
+}
+
+static void printf_f(float val)
+{
+    if (val < 0) {
+        printf_c('-');
+        val = -val;
+    }
+    int int_part = (int)val;
+    float frac_part = val - int_part;
+    printf_d(int_part);
+    printf_c('.');
+    for (int i = 0; i < 4; i++) {
+        frac_part *= 10;
+        int digit = (int)frac_part;
+        printf_c('0' + digit);
+        frac_part -= digit;
+    }
+}
+
 int printf(const char *format, ...)
 {
     int i;
     va_list ap;
-
     va_start(ap, format);
-
     for (i = 0; format[i]; i++)
         if (format[i] == '%')
         {
-            while (format[++i])
-            {
-                if (format[i] == 'c')
+            i++;
+            switch(format[i]) {
+                case 'c': printf_c(va_arg(ap, int)); break;
+                case 's': printf_s(va_arg(ap, char *)); break;
+                case 'd': printf_d(va_arg(ap, int)); break;
+                case 'u': printf_u(va_arg(ap, unsigned int)); break;
+                case 'x': printf_x(va_arg(ap, unsigned int), 0); break;
+                case 'X': printf_x(va_arg(ap, unsigned int), 1); break;
+                case 'f': printf_f(va_arg(ap, double)); break;
+                case 'l':
                 {
-                    printf_c(va_arg(ap, int));
-                    break;
+                    i++;
+                    switch(format[i]) {
+                        case 'd': printf_ld(va_arg(ap, long int)); break;
+                        case 'u': printf_lu(va_arg(ap, unsigned long int)); break;
+                        case 'x': printf_lx(va_arg(ap, unsigned long int), 0); break;
+                        case 'X': printf_lx(va_arg(ap, unsigned long int), 1); break;
+                        default: /* ignore unknown */ break;
+                    }
                 }
-                if (format[i] == 's')
-                {
-                    printf_s(va_arg(ap, char *));
-                    break;
-                }
-                if (format[i] == 'd')
-                {
-                    printf_d(va_arg(ap, int));
-                    break;
-                }
+                default: /* ignore unknown */ break;
             }
         }
         else
             printf_c(format[i]);
-
     va_end(ap);
 }
 
-int puts(char *s)
-{
-    while (*s)
-    {
-        putchar(*s);
-        s++;
-    }
-    putchar('\n');
-    return 0;
-}
-
-#endif /* CYBER_USART */
+#endif
