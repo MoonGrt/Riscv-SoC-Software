f552a1afd3196823073842109e09b99d1a40abd1
MoonGrt <1561145394@qq.com>
2026-02-09 03:07:32 +0800

    project delete test & add gitignore

--- projects/pika-python/test/pikapython/main.py
+++
@@ -1,5 +0,0 @@
-import PikaStdLib
-print('hello pikascript!')
-mem = PikaStdLib.MemChecker()
-print('mem used max:')
-mem.max()
--- projects/rt-thread/test/user/hw_timer.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TIMER_H__
-#define __TIMER_H__
-
-/* timer */
-extern void hw_timer_init();
-extern void hw_timer_irq_handler();
-
-#endif
